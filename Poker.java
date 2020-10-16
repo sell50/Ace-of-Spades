825124a6c81b0105b8e8053ad5c839c7034854a2
usman <usmannaveed012@gmail.com>
2020-10-15 21:00:23 -0400

    Just the main class amd the poker class (still working on the poker class)

---
+++ Poker.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+
+public class Poker {
+
+        DeckofCards pokerDeck;
+        Card [][] hands;// holds the players and AIs hands
+        int [][] amount = new int [6][2] ;//holds both the amount that the ai and player have to bet and the amount that is currently being bet
+
+        public Poker(){
+
+
+
+        }
+
+        public void startGame(){
+
+            boolean winCondition=false;
+
+            while(winCondition){
+
+            }
+        }
+
+
+        public void PlayerBets(){
+
+        }
+
+        public boolean endGame(){
+
+            return(false);
+        }
+
+
+
+}
