1d9391fe8aff220dc7303036ae00cc724af8d282
Kat <pacek@uwindsor.ca>
2020-11-17 13:41:48 -0500

    Update Crazy8s.java

--- Crazy8s.java
+++ Crazy8s.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 *Lose: Last player with any cards
 */
 
-public class Crazy8s {
+public class Crazy8sCopy{
     
     //Needed Objects
     DeckofCards deck;
@@ -21,10 +21,10 @@ public class Crazy8s {
     int currentDeckCard = 0;
     int tossCardID;
     int roundCounter = -2; //is this used?
-    boolean winner;
+    boolean winner = false;
     boolean newGame = true;
 
-    Crazy8s() throws Exception {
+    Crazy8sCopy() throws Exception {
 		
 		//Needed objects
 		discardPile = new Card[52];
@@ -36,6 +36,48 @@ public class Crazy8s {
     }
 
     public boolean startGame(){
+
+			/*//Needed variables
+		
+			boolean start = true;
+			int decision;
+			Crazy8sCopy crazy8s;
+			Scanner input = new Scanner(System.in);
+	
+			
+			while(start){
+				
+				//Prompt to start game
+				
+				System.out.println("Welcome to Crazy8s!");
+				System.out.println("Ready to start Crazy8s?");
+				System.out.println("Type 1 for YES, or 2 for NO: ");
+				decision = input.nextInt();
+			   
+				
+				//Start game
+				if(decision == 1){
+					crazy8s = new Crazy8sCopy(); 
+					start = crazy8s.startGame();
+				}
+					
+				else{
+					start = false;
+				}
+			}
+	
+			//end game
+		  
+			System.out.println("Thanks for playing!");
+		
+			input.close();
+	
+			
+	
+		}*/
+		//REAL ONE^^^
+		///////////////////////////////////////////////////////////
+		
 
 		//Needed variables
 		/*boolean start = true;
@@ -66,278 +108,80 @@ public class Crazy8s {
             }
 		}*/
 		
-		/*int decision;
-		Card tempCard;
-		Scanner input = new Scanner(System.in);*/
-	
-			
-			//First Round
-		if(checkSize(player) == 0 & newGame){
+	int rounds = 0;
+	if(newGame){
+		System.out.println("\nGame Start!\n");
+		newGame = false;
 
-			/*if(newGame == true & player.playerHand.Hand[0]==null & player.playerHand.Hand[1]==null & player.playerHand.Hand[2]==null & player.playerHand.Hand[3]==null & player.playerHand.Hand[4]==null)
-			{*/
-				firstemptyHand();
+		//Deal 5 cards from top of Deck to populate hand
+		DealCards(this.currentDeckCard,this.deck, this.player.playerHand);
 
-				/*System.out.println("Game Start!\n");
-				newGame = false;
+		discardPile[0] = deck.Deck[currentDeckCard];
 
-				//Deal 5 cards from top of Deck to populate hand
-				DealCards(this.currentDeckCard,this.deck, this.player.playerHand);
+		System.out.println("\nCurrent top card is " + discardPile[0].cardNumber + " of " + discardPile[0].cardSuit + "\n");
+	}
 
-				discardPile[0] = deck.Deck[currentDeckCard];
-
-				//RiggedHand();
-				//Card c2 = new Card(deck.cardNumbers[0], deck.cardSuits[0]);
-
-				//player.playerHand.DisplayHand();
-
-				//discardPile[0] = c2;
-
-				System.out.println("\n\tCurrent top card is " + discardPile[0].cardNumber + " of " + discardPile[0].cardSuit + "\n");
-
-				//Starting the next round
+	//Starting the next round
 				
-				startGame();*/
-	
-			
-			//}
-		}
-
 		//way to access varying hand lengths? functions on the bottom
-		for(int i = 0; i<15; i++){
+	while(rounds<=40){
+		int i = player.playerHand.Hand.length;
+
+		
+
 			if(checkSize(player) == i){
+
+				System.out.println("Number of cards in Hand: " + player.playerHand.Hand.length);
+
+				i = player.playerHand.Hand.length;
 				if(checkMatchNum(i, player)){
 					matchNum();
+					
 				}
+				i = player.playerHand.Hand.length;
 				if(checkMatchSuit(i, player)){
 					matchSuit();
+					
 				}
+				i = player.playerHand.Hand.length;
 				if(checkMatch8(i, player)){
 					match8();
+					
 				}
 
 				//If player does not have matching cards or 8
 				if(currentDeckCard!=deck.Deck.length)
 				{
-					System.out.println("You do not have any matching cards!\n");
+					System.out.println("\nYou do not have any matching cards!\n");
 					//Add new card....
 					player = addCard(this.currentDeckCard,this.deck, this.player);
+					
+					System.out.println("Adding new card!");
 	
-					System.out.println("\nYour cards: after adding");
+					System.out.println("\nYour cards after adding new card...");
 					player.playerHand.DisplayHand();
 					
 				}
-			}
-		}
 
-		//may delete this...
-			//If player has matching number card
-			/*
-			if(discardPile[0].cardNumber == player.playerHand.Hand[0].cardNumber || discardPile[0].cardNumber == player.playerHand.Hand[1].cardNumber || discardPile[0].cardNumber == player.playerHand.Hand[2].cardNumber || discardPile[0].cardNumber == player.playerHand.Hand[3].cardNumber || discardPile[0].cardNumber == player.playerHand.Hand[4].cardNumber)
-			{
-
-				matchNum();
-				/*
-				System.out.println("\nYou have a matching card number!");
-
-				System.out.println("Would you like to discard it?");
-				System.out.println("Type 1 for YES, or 2 for NO: \n");
-
-				decision = input.nextInt();
+				if(player.playerHand.Hand.length == 0)
+				{
 				
-
-				if(decision==1){
-
-					System.out.println("Discarding matching card now...\n");
-
-					//discard card...
-					for(int i = 0; i<player.playerHand.Hand.length; i++)
-						{
-							if(player.playerHand.Hand[i].cardNumber == discardPile[0].cardNumber){
-								
-								tempCard = player.playerHand.Hand[i];
-								tossCard(tempCard);
-
-								player = removeCards(player, i);
-
-								System.out.println("\nYour cards: after removing");
-								player.playerHand.DisplayHand();
-								break;
+				winner = emptyHandWin();
 	
-							}
-							
-						}
-					
-					System.out.println("\nbefore replacing card num\n");
-					
-					System.out.println("\nafter replacing card num\n");
+				break;
+				
+			
 				}
-
-				//Add new card 
-				if(decision == 2){
-					System.out.println("\nAdd new card cuz no match num\n");
-					//need to add here...
-					//replaceCard();
-					player = addCard(this.currentDeckCard,this.deck, this.player);
-
-					System.out.println("\nYour cards: after adding");
-					player.playerHand.DisplayHand();
-					startGame();
-				}*/
-			//}
-
-			//if matching card suit
-			/*
-			else if(discardPile[0].cardSuit.equals(player.playerHand.Hand[0].cardSuit) || discardPile[0].cardSuit.equals(player.playerHand.Hand[1].cardSuit) || discardPile[0].cardSuit.equals(player.playerHand.Hand[2].cardSuit) || discardPile[0].cardSuit.equals(player.playerHand.Hand[3].cardSuit) || discardPile[0].cardSuit.equals(player.playerHand.Hand[4].cardSuit))
-			{
-
-				matchSuit();
-
-				/*System.out.println("\nYou have a matching card suit!");
-
-				System.out.println("Would you like to discard it?");
-				System.out.println("Type 1 for YES, or 2 for NO: \n");
-
-				decision = input.nextInt();
-
-				if(decision==1){
-
-
-					System.out.println("Discarding matching card now...\n");
-
-					//discard card...
-					for(int i = 0; i<player.playerHand.Hand.length; i++)
-						{
-							if(player.playerHand.Hand[i].cardSuit.equals(discardPile[0].cardSuit)){
-								tempCard = player.playerHand.Hand[i];
-								tossCard(tempCard);
-
-								player = removeCards(player, i);
-
-								System.out.println("\nYour cards:");
-								player.playerHand.DisplayHand();
-
-								break;
-
-							}
-						}
-		
-					}
-				
-				//add new card
-				if(decision == 2)
-				{
-					System.out.println("\nAdd new card cuz no match suit\n");
-				
-					player = addCard(this.currentDeckCard,this.deck, this.player);
-
-					System.out.println("\nYour cards: after adding");
-					player.playerHand.DisplayHand();
-
-					startGame();
-				}*/
-			//}
-
-			
-			//If player has an 8 in pile (optional to use)
-			/*
-			if(player.playerHand.Hand[0].cardNumber == 8 || player.playerHand.Hand[1].cardNumber == 8 || player.playerHand.Hand[2].cardNumber == 8 || player.playerHand.Hand[3].cardNumber == 8 || player.playerHand.Hand[4].cardNumber == 8)
-			{
-				match8();
-
-				/*System.out.println("\nYour cards:");
-				player.playerHand.DisplayHand();
-				System.out.println("");
-				System.out.println("You have an 8 in your hand!");
-				System.out.println("Would you like to discard it?");
-				System.out.println("Type 1 for YES, or 2 for NO: \n");
-
-				decision = input.nextInt();
-
-				//tosses an 8
-				if(decision == 1)
-				{
-
-					System.out.println("Discarding matching card now...\n");
-
-					//discard 8
-					for(int i = 0; i<player.playerHand.Hand.length; i++)
-					{
-						if(player.playerHand.Hand[i].cardNumber == 8){
-							tempCard = player.playerHand.Hand[i];
-							tossCard(tempCard);
-
-							player = removeCards(player, i);
-
-							System.out.println("\nYour cards:");
-							player.playerHand.DisplayHand();
-							break;
-
-						}
-					}
-
-					
-				}
-
-				//Add new card
-				if(decision == 2)
-				{
-					System.out.println("\nreplace new card cuz no 8\n");
-					//need to add here...
-					//replaceCard();
-					player = addCard(this.currentDeckCard,this.deck, this.player);
-
-					System.out.println("\nYour cards: after adding");
-					player.playerHand.DisplayHand();
-					startGame();
-				}*/
-
-				
-			//}*/
-
-			/*
-			//If player does not have matching cards or 8
-			if(currentDeckCard!=deck.Deck.length)
-			{
-				System.out.println("You do not have any matching cards!\n");
-				//Add new card....
-				player = addCard(this.currentDeckCard,this.deck, this.player);
-
-				System.out.println("\nYour cards: after adding");
-				player.playerHand.DisplayHand();
-				
-			}*/
-			
-			//Win condition
-			if(!newGame & checkSize(player) == 0)
-			{
-				
-				emptyHandWin();
-				/*
-				System.out.println("\nCongrats! You won the game!");
-
-				roundCounter = 0;
-
-				winner = true;
-
-				 */
 			}
-			
-			//end game
-			/*
-			System.out.println("");
-			System.out.println("Thanks for playing!");
-			System.out.println("");
-			
-			input.close();*/
+			rounds++;
+	}
 
-			else{
-				System.out.println("new Round!\n");
-				startGame();
-			}
+	if(winner == false){
+		System.out.println("\nAI Won!");
 
-		return !winner;
-		
+	}
+
+	return winner;
 		
     }
 
@@ -366,11 +210,11 @@ public class Crazy8s {
 		else
 		{
 			//Display Hand to Player for Choice
-			System.out.println("Your cards:");
+			System.out.println("\nYour cards:");
 			Hand.DisplayHand();
 
 
-			System.out.println("\tCurrent top card is " + discardPile[0].cardNumber + " of " + discardPile[0].cardSuit + "\n");
+			System.out.println("\n\tCurrent top card is " + discardPile[0].cardNumber + " of " + discardPile[0].cardSuit + "\n");
 			
 			//Obtain the Card to be Tossed from Player
 			System.out.println("\nWhich Card Would You Like to Toss? (Pick a Number Between 1-5)");
@@ -387,10 +231,10 @@ public class Crazy8s {
 			//Updating the Global currentDeckCard
 			this.currentDeckCard = currentDeckCard;
 
-			System.out.println("\nYour cards: after toss");
+			System.out.println("\nYour cards after removing...");
 			Hand.DisplayHand();
 			
-			System.out.println("\ntop card after toss");
+			System.out.println("\ntop card after removing...");
 			System.out.println("\tCurrent top card is " + discardPile[0].cardNumber + " of " + discardPile[0].cardSuit + "\n");
 		}
 	}
@@ -399,7 +243,7 @@ public class Crazy8s {
 	{
 		int index = 0;
 		discardPile[index] = card;
-		System.out.println("\tCurrent top card is " + discardPile[index].cardNumber + " of " + discardPile[index].cardSuit);
+		System.out.println("\n\tCurrent top card is " + discardPile[index].cardNumber + " of " + discardPile[index].cardSuit);
 	}
 
 	public Player addCard(int currentDeckCard, DeckofCards deck, Player player){
@@ -420,7 +264,7 @@ public class Crazy8s {
 
 		}
 
-		tempHand.playerHand.Hand[length+1] = newCard;
+		tempHand.playerHand.Hand[length] = newCard;
 
 		return tempHand;
 
@@ -429,7 +273,7 @@ public class Crazy8s {
 
 	public Player removeCards(Player player, int cardIndex){
 
-		System.out.println("Element to be removed at index: " + cardIndex);
+		//System.out.println("Element to be removed at index: " + cardIndex);
 
 		if (player.playerHand == null || cardIndex < 0 || cardIndex >= player.playerHand.Hand.length) {
 			System.out.println("No removal operation can be performed!!");
@@ -453,29 +297,9 @@ public class Crazy8s {
 	}
 
 
-/*
-	public void RiggedHand()
-	{
-		for(int i = 0; i < 5; i++)
-		{
-			Card c1 = new Card(deck.cardNumbers[i+1], deck.cardSuits[0]);
-			player.playerHand.Hand[i] = c1;
-		}
-	}
-*/
-
-
 public int checkSize(Player player){
 
-	int i;
-
-	for(i = 0; i<=10; i++){
-		if(player.playerHand.Hand.length == i){
-			return i;
-		}
-	}
-
-	return i;
+	return player.playerHand.Hand.length;
 
 }
 
@@ -502,8 +326,9 @@ public void matchSuit(){
 				tossCard(tempCard);
 
 				player = removeCards(player, i);
+				
 
-				System.out.println("\nYour cards:");
+				System.out.println("\nYour cards after removing...");
 				player.playerHand.DisplayHand();
 
 				break;
@@ -515,16 +340,18 @@ public void matchSuit(){
 
 	// add new card
 	if (decision == 2) {
-		System.out.println("\nAdd new card cuz no match suit\n");
+		//System.out.println("\nAdd new card cuz no match suit\n");
 
 		player = addCard(this.currentDeckCard, this.deck, this.player);
 
-		System.out.println("\nYour cards: after adding");
+		System.out.println("\nYour cards after adding...");
 		player.playerHand.DisplayHand();
+
+		
 
 		startGame();
 	}
-	input.close();
+	
 }
 
 public void matchNum(){
@@ -552,7 +379,7 @@ public void matchNum(){
 
 				player = removeCards(player, i);
 
-				System.out.println("\nYour cards: after removing");
+				System.out.println("\nYour cards after removing...");
 				player.playerHand.DisplayHand();
 				break;
 
@@ -560,24 +387,20 @@ public void matchNum(){
 
 		}
 
-		System.out.println("\nbefore replacing card num\n");
-
-		System.out.println("\nafter replacing card num\n");
 	}
 
 	// Add new card
 	if (decision == 2) {
-		System.out.println("\nAdd new card cuz no match num\n");
-		// need to add here...
-		// replaceCard();
+		//System.out.println("\nAdd new card cuz no match num\n");
+
 		player = addCard(this.currentDeckCard, this.deck, this.player);
 
-		System.out.println("\nYour cards: after adding");
+		System.out.println("\nYour cards after adding...");
 		player.playerHand.DisplayHand();
 		startGame();
 	}
 
-	input.close();
+	
 }
 
 public void match8(){
@@ -586,8 +409,8 @@ public void match8(){
 	Scanner input = new Scanner(System.in);
 
 	System.out.println("\nYour cards:");
-	player.playerHand.DisplayHand();
-	//System.out.println("");
+	//player.playerHand.DisplayHand();
+	System.out.println("");
 	System.out.println("You have an 8 in your hand!");
 	System.out.println("Would you like to discard it?");
 	System.out.println("Type 1 for YES, or 2 for NO: \n");
@@ -598,7 +421,7 @@ public void match8(){
 	if(decision == 1)
 				{
 
-		System.out.println("Discarding matching card now...\n");
+		System.out.println("\nDiscarding matching card now...\n");
 
 		//discard 8
 		for(int i = 0; i<player.playerHand.Hand.length; i++)
@@ -609,7 +432,7 @@ public void match8(){
 
 					player = removeCards(player, i);
 
-					System.out.println("\nYour cards:");
+					System.out.println("\nYour cards after removing...");
 					player.playerHand.DisplayHand();
 					break;
 
@@ -621,16 +444,16 @@ public void match8(){
 	//Add new card
 	if(decision == 2)
 	{
-		System.out.println("\nreplace new card cuz no 8\n");
+		//System.out.println("\nreplace new card cuz no 8\n");
 		//need to add here...
 		//replaceCard();
 		player = addCard(this.currentDeckCard,this.deck, this.player);
 
-		System.out.println("\nYour cards: after adding");
+		System.out.println("\nYour cards after adding...");
 		player.playerHand.DisplayHand();
 		startGame();
 	}
-	input.close();
+	
 }
 
 public void firstemptyHand(){
@@ -643,23 +466,32 @@ public void firstemptyHand(){
 
 	discardPile[0] = deck.Deck[currentDeckCard];
 
+	//System.out.println("Size of hand: " + player.playerHand.Hand.length);
+
 	System.out.println("\n\tCurrent top card is " + discardPile[0].cardNumber + " of " + discardPile[0].cardSuit + "\n");
 
 	//Starting the next round
+
+	
 				
-	startGame();
+	//startGame();
 }
 
-public void emptyHandWin(){
+public boolean emptyHandWin(){
 
 	System.out.println("\nCongrats! You won the game!");
 
 	roundCounter = 0;
 
-	winner = true;
+	return false;
 }
 
 public boolean checkMatchNum(int n, Player player){
+
+	//System.out.println("Current hand length for checkNum: " + player.playerHand.Hand.length);
+	//System.out.println("Current value of n: " + n);
+
+	//System.out.println("Number of cards in Hand: " + n);
 
 	for(int i = 0; i<n; i++){
 		if(discardPile[0].cardNumber == player.playerHand.Hand[i].cardNumber){
@@ -672,6 +504,11 @@ public boolean checkMatchNum(int n, Player player){
 
 public boolean checkMatchSuit(int n, Player player){
 
+	//System.out.println("Current hand length for checkSuit: " + player.playerHand.Hand.length);
+	//System.out.println("Current value of n: " + n);
+
+	//System.out.println("Number of cards in Hand: " + n);
+
 	for(int i = 0; i<n; i++){
 		if(discardPile[0].cardSuit.equals(player.playerHand.Hand[i].cardSuit)){
 			return true;
@@ -683,6 +520,11 @@ public boolean checkMatchSuit(int n, Player player){
 
 public boolean checkMatch8(int n, Player player){
 	
+	//System.out.println("Current hand length for check8: " + player.playerHand.Hand.length);
+	//System.out.println("Current value of n: " + n);
+
+	//System.out.println("Number of cards in Hand: " + n);
+
 	for(int i = 0; i<n; i++){
 		if(player.playerHand.Hand[i].cardNumber == 8){
 			return true;
@@ -695,26 +537,26 @@ public boolean checkMatch8(int n, Player player){
 	public static void main(String[] args) throws Exception {
 
 		//Needed variables
+		
 		boolean start = true;
 		int decision;
-		Crazy8s crazy8s;
+		Crazy8sCopy crazy8s;
 		Scanner input = new Scanner(System.in);
 
-		//System.out.println("");
 		
 		while(start){
 			
 			//Prompt to start game
-			//System.out.println("");
+			
 			System.out.println("Welcome to Crazy8s!");
 			System.out.println("Ready to start Crazy8s?");
 			System.out.println("Type 1 for YES, or 2 for NO: ");
 			decision = input.nextInt();
-            //System.out.println("");
+           
             
 			//Start game
 			if(decision == 1){
-				crazy8s = new Crazy8s(); //help???
+				crazy8s = new Crazy8sCopy(); 
 				start = crazy8s.startGame();
             }
                 
@@ -724,10 +566,9 @@ public boolean checkMatch8(int n, Player player){
 		}
 
 		//end game
-        //System.out.println("");
+      
         System.out.println("Thanks for playing!");
-		//System.out.println("");
-		
+	
 		input.close();
 
 		
